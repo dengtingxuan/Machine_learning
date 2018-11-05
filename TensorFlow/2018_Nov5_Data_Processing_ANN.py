37d7621fda51ff8e158fd43a6712a06c9f1d090c
Tony Deng <39846534+dengtingxuan@users.noreply.github.com>
2018-11-05 16:11:23 +0800

    Add files via upload

---
+++ TensorFlow/2018_Nov5_Data_Processing_ANN.py
@@ -0,0 +1,34 @@
+"""This code is for experimenting to build a model of predicting window opening behaviors"""
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+data=pd.read_csv("F:/201810_ML_Learning\All_Data/Sum_OF_All_Data_KMJ1_7.csv")
+print(data.columns)
+x=data[['Hour', 'Month', 'Week', 'Temperature', 'RH', 'HCHO', 'CO2', 'PM2.5','Out_Temperature',
+        'Out_RH', 'Rain', 'CO(mg/m3)', 'NO2(ug/m3)','SO2(ug/m3)', 'O3(ug/m3)', 'PM10(ug/m3)',
+        'PM2.5(ug/m3)', 'AQI']]
+y=data['BRW']
+y_n=pd.DataFrame()
+a0=pd.DataFrame({"a":[1],"b":[0]},columns=["a","b"])
+a1=pd.DataFrame({"a": [0], "b": [1]}, columns=["a", "b"])
+
+print(x.shape)
+print(y.shape)
+
+for i in range(len(y)):
+    if y.iloc[i]==1:
+        y_n= y_n.append(a0)
+    else:
+        y_n = y_n.append(a1)
+    print(i)
+print(y_n)
+
+y_n.to_csv("F:/201810_ML_Learning/Tensorflow/Temp_ALL_KMJ1_7_data_Handled_For_TF_win.csv")
+y_n=pd.read_csv("F:/201810_ML_Learning/Tensorflow/Temp_ALL_KMJ1_7_data_Handled_For_TF_win.csv")
+data_n=pd.concat((x,y_n),axis=1)
+print(data_n)
+data_n.to_csv("F:/201810_ML_Learning/Tensorflow/ALL_KMJ1_7_data_Handled_For_TF_win.csv")
